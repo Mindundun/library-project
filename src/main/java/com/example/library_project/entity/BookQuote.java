db9b31b66b18130e938601f856b051d573005142
Mindun <pmk10077@gmail.com>
2025-10-24 17:34:29 +0900

    책 속 문장 엔티티 추가(추후 기능추가예정)

---
+++ src/main/java/com/example/library_project/entity/BookQuote.java
@@ -0,0 +1,26 @@
+package com.example.library_project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+// 책 속 문장
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookQuote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
